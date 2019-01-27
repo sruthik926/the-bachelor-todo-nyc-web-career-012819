991ff24fa9809c9332c087d446965fb9b31d4565
Sruthi Krishna <sruthik926@gmail.com>
2019-01-27 23:16:09 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,19 @@
 require 'pry'
 
 def get_first_name_of_season_winner(data, season)
-       data[season].each do |info_hash|
+    data[season].each do |info_hash|
          if info_hash["status"] == "Winner"
             return info_hash["name"].split(" ")[0]
          end
-       end
+    end
 end
 
 def get_contestant_name(data, occupation)
   data.each do |season, season_data|
       season_data.each do |contestant_hash|
           contestant_hash.each do |con_key, con_value|
-            if con_key == occupation
-                binding.pry
+            if con_value == occupation
+              return contestant_hash["name"]
             end
           end
       end
